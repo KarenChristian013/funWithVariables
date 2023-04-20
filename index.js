48d0c88be036173f2b358b6c8b11afb2c7668c6f
KarenChristian <karen.lewis013@yahoo.com>
2023-04-19 20:23:08 -0400

    homeworkVariables

---
+++ index.js
@@ -0,0 +1,70 @@
+var a = 1
+var b = 2
+var c = 3
+var d = 4
+var e = 5
+var f = 6
+var g = 7
+var h = 8
+var i = 9
+var j = 10
+
+function sum(a, b){
+    return a+b;
+};
+ var result = sum(1, 2);
+console.log(result);
+
+function sum(f, h){
+    return f+h;
+};
+ var result = sum(f, h);
+console.log(result);
+
+function sub(j, e){
+    return j - e;
+};
+ var result = sub(j, e);
+console.log(result);
+
+function sub(h, d){
+    return h - d;
+};
+ var result = sub(h, d);
+console.log(result);
+
+function multiply(c, h){
+    return c * h;
+};
+ var result = multiply(c, h);
+console.log(result);
+
+function multiply(e, d){
+    return e * d;
+};
+ var result = multiply(e, d);
+console.log(result);
+
+function division(i, c){
+    return i / c;
+};
+ var result = division(i, c);
+console.log(result);
+
+
+var myVar = j;
+myVar++;
+
+var myVar = h;
+myVar++;
+
+var myVar = c;
+myVar--;
+
+var myVar = e;
+myVar--;
+
+
+
+
+
